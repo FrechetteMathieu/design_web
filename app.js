1ac712d7babcf76a9f0aac5527f676cb8a45a1e3
Mathieu Fréchette <frechette.mathieu@cegepvicto.ca>
2024-11-11 10:04:10 -0500

    Add static folder access

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 3000;
 // Middleware pour parser les données JSON et les données de formulaire
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// Middleware pour servir les fichiers statiques
+app.use(express.static(__dirname + '/public'));
+console.log(__dirname + '/public');
 
 const submitRoutes = require('./src/routes/submit.route');
 app.use('/submit', submitRoutes);
